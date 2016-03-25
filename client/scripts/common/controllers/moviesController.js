63aca2949a7652b37168a3e4dca7a1314250ff8b
hassony2 <yanahasson@gmail.com>
2016-03-25 14:53:07 +0300

    feat(app): Link movieCtrl to input text

--- client/scripts/common/controllers/moviesController.js
+++ client/scripts/common/controllers/moviesController.js
@@ -10,12 +10,15 @@ module.exports = function(app) {
     function controller(moviesFetcher, $log) {
         var that = this;
         that.searchString = '';
-        that.getMovies = function(string) {
-
-            moviesFetcher.getMovies('test').then(function(response) {
-                that.movieList = response;
-                $log.debug(response);
-            });
+        that.getMovies = function() {
+            if (!that.searchString) {
+                $log.debug('give me something to search');
+            } else {
+                moviesFetcher.getMovies(that.searchString).then(function(response) {
+                    that.movieList = response;
+                    $log.debug(response);
+                });
+            }
         };
     }
     var activate = function() {};
